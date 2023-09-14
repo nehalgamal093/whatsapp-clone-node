17795c81d552494902d189818038bdd0424a0402
nehalgamal093 <nehalgamal093@gmail.com>
2023-09-14 15:05:10 +0300

    deploy

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const express = require('express');
 var http = require("http");
 
 const app = express();
-const port =  5000;
+const port =  3000;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 //middleware
 app.use(express.json());
-
+var clients ={}
 io.on('connection',(socket)=>{
     console.log('connected')
     console.log(socket.id, 'has joined');
-    socket.on("/test",(msg)=>{
+    socket.on("signin",(id)=>{
+        console.log(id);
+        clients[id]=socket;
+        console.log(clients);
+    })
+    socket.on("message",(msg)=>{
         console.log(msg);
+        let targetId = msg.targetId;
+        if(clients[targetId]) clients[targetId].emit("message",msg)
     })
 });
+
 app.get("/", (req, res) => {
     res.send("Hello world");
   });
